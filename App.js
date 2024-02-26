0d5f7d8c414af599b339924b7b00aea66b9857e0
JACL613 <cuervoandres1213@gmail.com>
2024-02-25 22:50:58 -0500

    commit base

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View , Button, Alert} from 'react-native';
+// import {useFonts, Inter_100Thin , Inter_600SemiBold} from '@expo-google-fonts/inter'
+import {useFonts, Roboto_300Light , Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
+  // let [fontsLoaded, fontError] = useFonts({
+  //   Inter_100Thin,
+  //   Inter_600SemiBold
+  // });
+  let [fontsLoaded, fontError] = useFonts({
+   Roboto_300Light, 
+   Roboto_700Bold
+  });
+  const [Verification , setVerefication] = useState(false)
+  const createTwoButtonAlert = () =>
+  Alert.alert('Alert Title', 'My Alert Msg', [
+    {
+      text: 'Cancel',
+      onPress: () => setVerefication(!Verification),
+      style: 'cancel',
+    },
+    {text: 'OK', onPress: () => setVerefication(!Verification)},
+  ]);
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Pomodoro</Text>
+      <Button title='Hello' onPress={createTwoButtonAlert} />
+      <Text>{Verification ? 'Vereficado' : 'No Vereficado'}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +43,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+   fontSize:24,
+   fontFamily: 'Roboto_700Bold|',
+  }
 });
